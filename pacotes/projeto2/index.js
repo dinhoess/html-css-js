6b50cbb5ac8cf01d4ea8fd7c418ef5f9ec8c9733
Edvaldo_Silva <dinhoess@yahoo.com.br>
2025-01-21 07:47:48 -0300

    banco v.1 finalizada

--- pacotes/projeto2/index.js
+++ pacotes/projeto2/index.js
@@ -1,18 +1,33 @@
 import pensador from "pensador-scrap";
+import fs from "fs";
 
-const {error,success} = await pensador.search(
+const autor = "Mario Sergio Cortela";
+
+let pensamentoParaSalvar;
+
+const { error, success } = await pensador.search(
     {
-        query: "Dalai Lama",
-        limit:12
+        query: autor,
+        limit: 12
     })
-   
 
-    // console.log(error);
-    // console.log(success);
+// console.log(error);
+// console.log(success);
+try {
+    const pensamentos = success.thought;
+    const indiceAleatorio = Math.floor(Math.random() * pensamentos.length);
+    const pensamentoSelecionado = pensamentos[indiceAleatorio].content;
 
-const pensamentos = success.thought;
-const indiceAleatorio = Math.floor(Math.random() * pensamentos.length);
-const pensamentoSelecionado = pensamentos[indiceAleatorio].content; 
+    pensamentoParaSalvar = `Pensamento de ${autor} -> ${pensamentoSelecionado}\n`;
+} catch (e) {   
+    pensamentoParaSalvar = `${autor} não tem pensamentos! \n`
+} finally{
 
+    //writeFileSync("frases.txt",pensamentoParaSalvar,"utf8")
+    fs.appendFileSync("frases.txt", pensamentoParaSalvar, "utf8");
+    
+    const texto = fs.readFileSync("frases.txt", "utf8")
+}
+//console.log(pensamentoParaSalvar);
 
-console.log(pensamentoSelecionado);
+// console.log(texto);
